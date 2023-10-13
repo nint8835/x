6d40721df48e9034cbed7aff02b3b5df170bfbf2
Riley Flynn <riley@rileyflynn.me>
2023-10-12 23:58:06 -0230

    Basic parsing & CSV export

--- mun_grades/process.py
+++ mun_grades/process.py
@@ -1,12 +1,15 @@
 import pathlib
+import re
+import csv
 
 import ocrmypdf
 
 unprocessed_reports_path = pathlib.Path("./reports")
 processed_reports_path = pathlib.Path("./processed_reports")
 sidecars_path = pathlib.Path("./sidecars")
+csvs_path = pathlib.Path("./csvs")
 
-for directory in [unprocessed_reports_path, processed_reports_path, sidecars_path]:
+for directory in [unprocessed_reports_path, processed_reports_path, sidecars_path, csvs_path]:
     directory.mkdir(exist_ok=True)
 
 for path in unprocessed_reports_path.iterdir():
@@ -26,3 +29,23 @@ for path in unprocessed_reports_path.iterdir():
         # Causes tesseract to not try to detect columns
         tesseract_pagesegmode=6,
     )
+
+# WIP parsing regex. Still has known issues:
+# - Doesn't handle courses with no professor listed
+# - Doesn't handle courses with letters in course number (such as 499A)
+# - Doesn't handle courses with OCR artifacts in the string
+#   - This might be a fix on the OCR side, rather than the regex side :person_shrugging:
+course_regex = re.compile(r"(\d{6}) ([\S ]+ [\dX]{4})(?:\S*) ([\S ]+) (\S+, [\S ]+) (PASS|FAIL|PWD|A|B|C|D|F)(?: (\d+) (\d+))?")
+
+for sidecar_path in sidecars_path.iterdir():
+    print(f"Processing {sidecar_path.name}")
+
+    with open(sidecar_path) as sidecar:
+        sidecar_contents = sidecar.read()
+    
+    with open(csvs_path / sidecar_path.name.replace(".txt", ".csv"), "w") as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(["Term", "Course Number", "Course Title", "Professor", "Grade", "Grade Count", "Total Enrolled"])
+
+        for match in course_regex.finditer(sidecar_contents):
+            csv_writer.writerow(match.groups())
